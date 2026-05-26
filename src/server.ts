26402a7a6f289774d48c2390ba6c914c0e9a8ea4
SAYAN02-DEV <sayan2003.dev@gmail.com>
2026-05-26 11:46:12 +0530

    API: app and server entry points created

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import http from 'http'
+import app from './app'
+import { connectDatabase, env } from './config'
+import { createSocketServer } from './socket'
+import { notificationService } from './services/notification.service'
+import { logger } from './utils/logger'
+
+const start = async () => {
+  await connectDatabase()
+
+  const httpServer = http.createServer(app)
+  const io = await createSocketServer(httpServer)
+  notificationService.init(io)
+
+  httpServer.listen(env.PORT, () => {
+    logger.info(`API server running on port ${env.PORT}`)
+  })
+
+  // graceful shutdown
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM received, shutting down')
+    httpServer.close(() => process.exit(0))
+  })
+}
+
+start()
